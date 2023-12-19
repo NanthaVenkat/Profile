048a85f3027b56b5897a00124c61c6e331fc2c9e
nantha@6storage <nanthakumar.v@6storage.com>
2023-12-19 17:43:39 +0530

    Porfolio Project

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
+// toggle bar 
 const togglebutton = document.getElementsByClassName("toggle-button")[0];
 const navbarlinks = document.getElementsByClassName("nav-links")[0];
 
 togglebutton.addEventListener( "click", ()=> {
-    navbarlinks.classList.toogle('active')
+    navbarlinks.classList.toggle('active')
 } )
+
+// nav color change by scrolling.
+
+const navbar = document.querySelector(".navbar");
+const navLink = document.querySelector(".nav-links");
+const toggleButton = document.querySelector(".toggle-button")
+
+window.addEventListener( 'scroll', ()=> {
+    const scrollPos = window.scrollY;
+
+    if (scrollPos > 0) {
+        navbar.classList.add("scroll");
+        navLink.classList.add("scroll");
+        toggleButton.classList.add("scroll");
+    }
+    else {
+        navbar.style.backgroundColor = "##2b7a78";
+        navbar.classList.remove("scroll");
+        navLink.classList.remove("scroll");
+        toggleButton.classList.remove("scroll");
+    }
+})
+
+function downloadPDF() {
+    // Replace 'path/to/your/file.pdf' with the actual path to your PDF file.
+    const pdfPath = 'resume/resume.pdf';
+
+    // Create an anchor element
+    const anchor = document.createElement('a');
+
+    // Set the href attribute to the PDF file path
+    anchor.href = pdfPath;
+
+    // Set the download attribute with the desired file name
+    anchor.download = 'Nkresume.pdf';
+
+    // Append the anchor to the body
+    document.body.appendChild(anchor);
+
+    // Trigger a click event on the anchor
+    anchor.click();
+
+    // Remove the anchor from the body
+    document.body.removeChild(anchor);
+}
